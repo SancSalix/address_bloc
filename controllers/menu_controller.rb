134adf81ea92ad75cdceea919d794593fa8f8e0d
Shelby <shelbcoleman@gmail.com>
2017-06-29 20:47:13 +0000

    Adding more menu functionality

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -60,7 +60,10 @@
      case selection
        when "n"
        when "d"
+        delete_entry(entry)
        when "e"
+         edit_entry(entry)
+         entry_submenu(entry)
        when "m"
         system "clear"
         main_menu
@@ -96,6 +99,16 @@
    end
  
    def search_entries
+     print "Search by name: "
+     name = gets.chomp
+     match = address_book.binary_search(name)
+     system "clear"
+     if match
+       puts match.to_s
+       search_submenu(match)
+     else
+       puts "No match found for #{name}"
+     end
    end
  
    def read_csv
@@ -131,6 +144,59 @@
      else
          puts "Please enter valid number"
          display_entry
+     end
+   end
+   
+   def delete_entry(entry)
+     address_book.entries.delete(entry)
+     puts "#{entry.name} has been deleted"
+   end
+   
+   def detonate_book()
+     address_book.entries.each do |entry|
+     address_book.entries.delete(entry)
+    end
+     puts "All entries have been deleted"
+   end
+   
+   
+   def edit_entry(entry)
+     print "Updated name: "
+     name = gets.chomp
+     print "Updated phone number: "
+     phone_number = gets.chomp
+     print "Updated email: "
+     email = gets.chomp
+     entry.name = name if !name.empty?
+     entry.phone_number = phone_number if !phone_number.empty?
+     entry.email = email if !email.empty?
+     system "clear"
+     puts "Updated entry:"
+     puts entry
+   end
+    
+   def search_submenu(entry)
+     puts "\nd - delete entry"
+     puts "e - edit this entry"
+     puts "m - return to main menu"
+     selection = gets.chomp
+     case selection
+       when "d"
+         system "clear"
+         delete_entry(entry)
+         main_menu
+       when "e"
+         edit_entry(entry)
+         system "clear"
+         main_menu
+       when "m"
+         system "clear"
+         main_menu
+       else
+         system "clear"
+         puts "#{selection} is not a valid input"
+         puts entry.to_s
+         search_submenu(entry)
      end
    end
 
